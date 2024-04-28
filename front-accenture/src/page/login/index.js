f6bedd3bdc55a477b4c9b80733edda3763317d12
Wallace Martins <martinsswallace@gmail.com>
2024-04-28 19:42:10 -0300

    chore: ajuste removendo navbar

--- front-accenture/src/page/login/index.js
+++ front-accenture/src/page/login/index.js
@@ -1,12 +1,10 @@
 import './index.css';
 import googleIcon from '../../assets/google-img.svg'
 import facebookIcon from '../../assets/facebook-img.svg'
-import Navbar from '../../components/navbar';
 
-function Login(){
-    return(
+function Login() {
+    return (
         <div className='container'>
-            <Navbar />
             <div className='container-login'>
                 <form className='login-form'>
                     <div className='login-form-title'>
@@ -15,22 +13,22 @@ function Login(){
 
                     <div className='wrap-input'>
                         <span>Login</span>
-                        <input className='input' type='email' placeholder='Digite seu email'/>
+                        <input className='input' type='email' placeholder='Digite seu email' />
                     </div>
 
                     <div className='wrap-input'>
                         <span>Senha</span>
-                        <input className='input' type='password' placeholder='Digite sua senha'/>
+                        <input className='input' type='password' placeholder='Digite sua senha' />
                     </div>
-                    
+
                     <div className='login-form-button-forgot'>
-                        <button className='forgot-password'> 
+                        <button className='forgot-password'>
                             <span>Esqueceu sua senha?</span>
                         </button>
                     </div>
 
                     <div className='login-form-button'>
-                        <button className='login-form-btn'> 
+                        <button className='login-form-btn'>
                             Continuar
                         </button>
                     </div>
@@ -42,16 +40,16 @@ function Login(){
                     </div>
 
                     <div className='social-buttons'>
-                        <button className='google-btn'> 
-                            <img src={googleIcon} alt='Google icon'/>
+                        <button className='google-btn'>
+                            <img src={googleIcon} alt='Google icon' />
                         </button>
-                        <button className='facebook-btn'> 
-                            <img src={facebookIcon} alt='Facebook icon'/>
+                        <button className='facebook-btn'>
+                            <img src={facebookIcon} alt='Facebook icon' />
                         </button>
                     </div>
 
                     <div className='login-form-button-register'>
-                        <button className='register'> 
+                        <button className='register'>
                             <span>Não tem conta? <span>Cadastre aqui!</span></span>
                         </button>
                     </div>
