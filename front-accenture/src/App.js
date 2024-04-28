5be41399e6707cd5a6d7e5e62fb41b286a9d487a
Wallace Martins <martinsswallace@gmail.com>
2024-04-28 19:41:39 -0300

    feat: adicionando navbar

--- front-accenture/src/App.js
+++ front-accenture/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './page/login'
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path="/login" Component={Login} /> 
         </Routes>
