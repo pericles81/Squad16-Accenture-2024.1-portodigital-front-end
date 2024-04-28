2ee9f67fe0bb227b2cbf7cece870206f5da1f7a7
Wallace Martins <martinsswallace@gmail.com>
2024-04-28 19:27:11 -0300

    feat: início ao controle de rotas

--- front-accenture/src/App.js
+++ front-accenture/src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './page/login'
 
 function App() {
   return (
     <div className="App">
-     <h1>ladrao</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" Component={Login} /> 
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
