19e9cba1f982b845e2d5150b2e77f5d2a95a171a
Wallace Martins <martinsswallace@gmail.com>
2024-05-27 00:10:06 -0300

    chore: ajustando para aparecer os produtos

--- front-accenture/src/page/home/Home.jsx
+++ front-accenture/src/page/home/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Button } from '../../components/Button'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button } from '../../components/Button';
 import { Card } from '../../components/Card';
-import styles from './Home.css'
+import styles from './Home.css';
+
 export const Home = () => {
     const [data, setData] = useState([]);
 
@@ -12,7 +12,6 @@ export const Home = () => {
             try {
                 const response = await axios.get('http://localhost:8080/produtos');
                 setData(response.data);
-                console.log(data);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -23,34 +22,32 @@ export const Home = () => {
     return (
         <div className='divMain'>
             <div>
-                <img className='imgMain' src="https://339028l.ha.azioncdn.net/img/2023/07/produto/29308/abajur-murano-cristal-1chatelet-garnet-ambientada-sottile-casa-1.jpg" alt="" />
+                <img className='imgMain' src="https://marketplace.canva.com/EAFuKwSatM8/1/0/1600w/canva-banner-para-loja-online-frete-gr%C3%A1tis-com-desconto-roxo-e-branco-kdzISsOH_AA.jpg" alt="" />
             </div>
+
             <div className='divButtons'>
-                <Button divClassName='divButton' classInputName='input' placeholder='Qual produto você deseja?'></Button>
-
-                <Button divClassName='divButton' classInputName='inputBtn' type='button' value='Buscar produto'></Button>
+                <Button divClassName='divButton' classInputName='input' placeholder='Qual produto você deseja?' />
+                <Button divClassName='divButton' classInputName='inputBtn' type='button' value='Buscar produto' />
             </div>
 
-            {data.length ? (<>
-                <div className="divCards" >
-                    {data.map((e) => {
-                        return (
-                            <Card
-                                divMainCard='divCard'
-                                img='img'
-                                divImg='divImg'
-                                h2={e.nome}
-                                text1={e.preco}
-                                text2={e.quantidade}
-                                text3={e.descricao}
-                            ></Card>
-                        )
-                    })}
-                </div></>) : 
-                <>
-                    <h3>AINDA NAO EXISTEM PRODUTOS CADASTRADOS </h3>
-                </>
-            }
+            {data.length ? (
+                <div className="divCards">
+                    {data.map((produto) => (
+                        <Card
+                            key={produto.id}
+                            divMainCard='divCard'
+                            img='img'
+                            divImg='divImg'
+                            h2={produto.nome}
+                            text1={`Preco: ${produto.preco}`}
+                            text2={`Descrição: ${produto.descricao}`}
+                            text3={`Quantidade: ${produto.quantidade}`}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <h3>AINDA NÃO EXISTEM PRODUTOS CADASTRADAS</h3>
+            )}
         </div>
-    )
-}
+    );
+};
