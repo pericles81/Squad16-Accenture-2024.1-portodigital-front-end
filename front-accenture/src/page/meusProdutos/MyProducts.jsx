f36fdcbab7754c0e0c3dc856cdd37c3b1ec0bb53
Wallace Martins <martinsswallace@gmail.com>
2024-06-03 02:03:47 -0300

    chore: ajustes na page de meus produtos

--- front-accenture/src/page/meusProdutos/MyProducts.jsx
+++ front-accenture/src/page/meusProdutos/MyProducts.jsx
@@ -5,11 +5,19 @@ import axios from 'axios';
 export const MyProducts = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const cliente = parseInt(localStorage.getItem('userId'), 10);
+        setIsLoggedIn(cliente);
+
+        if (!isLoggedIn) {
+          setIsLoading(false); 
+          return; 
+        }
+
         const url = cliente ? 'http://localhost:8080/produtos/cliente/' : 'http://localhost:8080/produtos/loja/';
 
         const response = await axios.get(url + cliente);
@@ -20,7 +28,7 @@ export const MyProducts = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [isLoggedIn]);
 
   if (isLoading) {
     return <div>Carregando...</div>;
@@ -29,23 +37,27 @@ export const MyProducts = () => {
   return (
     <>
       {
-        data.length ? (
-          <div className="divCards">
-            {data.map((produto) => (
-              <Card
-                key={produto.id}
-                divMainCard='divCard'
-                img='img'
-                divImg='divImg'
-                h2={produto.nome}
-                text1={`Preco: ${produto.preco}`}
-                text2={`Descrição: ${produto.descricao}`}
-                text3={`Quantidade: ${produto.quantidade}`}
-              />
-            ))}
-          </div>
+        isLoggedIn ? (
+          data.length ? (
+            <div className="divCards">
+              {data.map((produto) => (
+                <Card
+                  key={produto.id}
+                  divMainCard='divCard'
+                  img='img'
+                  divImg='divImg'
+                  h2={produto.nome}
+                  text1={`Preco: ${produto.preco}`}
+                  text2={`Descrição: ${produto.descricao}`}
+                  text3={`Quantidade: ${produto.quantidade}`}
+                />
+              ))}
+            </div>
+          ) : (
+            <h3>Não existem produtos cadastrados.</h3>
+          )
         ) : (
-          <h3>AINDA NÃO EXISTEM PRODUTOS CADASTRADOS</h3>
+          <h3>Você não está logado. Por favor, faça login para ver seus produtos.</h3>
         )
       }
     </>
