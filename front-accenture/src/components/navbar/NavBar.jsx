b4b8b3486596608a5b419e15d7c0ec95beb73274
Wallace Martins <martinsswallace@gmail.com>
2024-06-03 01:53:12 -0300

    chore: ajuste navbar

--- front-accenture/src/components/navbar/NavBar.jsx
+++ front-accenture/src/components/navbar/NavBar.jsx
@@ -1,43 +1,66 @@
-import './index.css'
-import React from 'react';
+import './index.css';
+import React, { useEffect, useId, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Button } from './../Button';
+
 function Navbar() {
-
   const location = useLocation();
-  const page = location.pathname
+  const page = location.pathname;
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    setIsLoggedIn(!!userId);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('userId');
+    setIsLoggedIn(false);
+  };
 
 
-if(page == '/login' || page == '/cadastroClientes' || page == '/seleCad'){  return (
-    <nav className="navbar">
-      <div className="logo">
-        <img alt="Logo" />
-      </div>
-      <div className="links">
-        <Link to="/login">Login</Link>
-        <Link to="/seleCad">Cadastrar-se</Link>
-      </div>
-    </nav>
-    )
-  }else if(page == '/lojas' || page == '/' || page== '/carrinho' || '/loja/:id'){
+  if (page === '/login' || page === '/cadastroClientes' || page === '/seleCad') {
     return (
       <nav className="navbar">
         <div className="logo">
           <img alt="Logo" />
         </div>
         <div className="links">
-          <Link to="/lojas">Lojas</Link>
-          <Link to="/">Meus Anúncios</Link>
-          <Link to="/carrinho">Carrinho</Link>
-          <Link to="/seleCad"><Button type='button'
-          value='Anunciar'></Button></Link>
-          
+          <Link to="/login">Login</Link>
+          <Link to="/seleCad">Cadastrar-se</Link>
         </div>
       </nav>
-      )
+    );
+  } else if (page === '/lojas' || page === '/' || page === '/carrinho' || '/loja/:id') {
+    return (
+      <nav className="navbar">
+        <div className="logo">
+          <img alt="Logo" />
+        </div>
+        <div className="links">
+          <Link to="/">Produtos</Link>
+          <Link to="/lojas">Lojas</Link>
+          <Link to="/carrinho">Carrinho</Link>
+          <Link to="/meusProdutos">Meus Produtos</Link>
+          {isLoggedIn ? (
+            <>
+              <Link to="/produtos">
+                <Button type="button" value="Anunciar"/>
+              </Link>
+              <Button type="button" value="Deslogar" onClick={handleLogout} />
+            </>
+
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/seleCad">Cadastrar-se</Link>
+            </>
+          )}
+        </div>
+      </nav>
+    );
   }
-
-
 }
 
 export default Navbar;
